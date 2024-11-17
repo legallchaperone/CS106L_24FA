03bbdb0007b8019120132a9c71c4b2b02d7d06e0
simoncodespace <simonchen24@sjtu.edu.cn>
2024-11-17 08:35:25 +0000

    feat: CMake

---
+++ build/CMakeFiles/cs106l.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/workspaces/CS106L_24FA/cs106l-assignments/assign1/main.cpp" "/workspaces/CS106L_24FA/build/CMakeFiles/cs106l.dir/cs106l-assignments/assign1/main.cpp.o"
+  "/workspaces/CS106L_24FA/cs106l-assignments/assign1/utils.cpp" "/workspaces/CS106L_24FA/build/CMakeFiles/cs106l.dir/cs106l-assignments/assign1/utils.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/cs106l.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cs106l.dir/cs106l-assignments/assign1/main.cpp.o"
+  "CMakeFiles/cs106l.dir/cs106l-assignments/assign1/utils.cpp.o"
+  "cs106l"
+  "cs106l.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cs106l.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
